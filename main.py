0690eca5781665bb62e5d436541a32340917d877
Дмитрий Соловьёв <sdmitriyyu@gmail.com>
2024-11-05 20:28:42 +0300

    Изменение набора символов для ASCII арта из изображений

---
+++ main.py
@@ -0,0 +1,134 @@
+import telebot
+from PIL import Image
+import io
+from telebot import types
+from conf import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+user_states = {}  # Хранение информации о пользователях
+
+
+# Обработчик команды /start и /help
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Пожалуйста, отправь мне фото.")
+
+
+# Обработчик фото
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    photo = message.photo[-1]  # Берем самое большое фото
+    file_info = bot.get_file(photo.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    image_stream = io.BytesIO(downloaded_file)
+
+    # Сохраняем информацию о фото
+    user_states[message.chat.id] = {'photo': photo.file_id, 'ascii_chars': None}
+
+    # Запрашиваем набор символов для ASCII-арта
+    bot.reply_to(message, "Введите набор символов, который будет использоваться для создания ASCII-арта:")
+    bot.register_next_step_handler(message, set_ascii_chars)
+
+
+# Функция для установки пользовательских символов
+def set_ascii_chars(message):
+    user_states[message.chat.id]['ascii_chars'] = message.text
+    bot.reply_to(message, "Ваш набор символов сохранен! Выберите действие:", reply_markup=get_options_keyboard())
+
+
+# Функция создания клавиатуры с опциями
+def get_options_keyboard():
+    keyboard = types.InlineKeyboardMarkup()
+    pixelate_btn = types.InlineKeyboardButton("Pixelate", callback_data="pixelate")
+    ascii_btn = types.InlineKeyboardButton("ASCII Art", callback_data="ascii")
+    keyboard.add(pixelate_btn, ascii_btn)
+    return keyboard
+
+
+# Обработчик нажатий на кнопки клавиатуры
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "pixelate":
+        bot.answer_callback_query(call.id, "Огрубляем ваше изображение...")
+        pixelate_and_send(call.message)
+    elif call.data == "ascii":
+        bot.answer_callback_query(call.id, "Преобразуем ваше изображение в ASCII арт...")
+        ascii_and_send(call.message)
+
+
+# Функция преобразования изображения в ASCII и отправки пользователю
+def ascii_and_send(message):
+    photo_id = user_states[message.chat.id]['photo']
+    file_info = bot.get_file(photo_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    image_stream = io.BytesIO(downloaded_file)
+    ascii_chars = user_states[message.chat.id]['ascii_chars']
+
+    if ascii_chars:
+        src_ascii_art = image_to_ascii(image_stream, ascii_chars)
+        bot.send_message(message.chat.id, f"<pre>{src_ascii_art}</pre>", parse_mode='HTML')
+    else:
+        bot.send_message(message.chat.id, "Набор символов не установлен.")
+
+
+# Функция преобразования изображения в ASCII с использованием пользовательских символов
+def image_to_ascii(image_stream, ascii_chars, new_width=40):
+    image = Image.open(image_stream).convert('L')
+    width, height = image.size
+    aspect_ratio = height / float(width)
+    new_height = int(aspect_ratio * new_width * 0.55)
+    img_resized = image.resize((new_width, new_height))
+
+    img_str = pixels_to_ascii(img_resized, ascii_chars)
+    img_width = img_resized.width
+
+    max_characters = 4000 - (new_width + 1)
+    max_rows = max_characters // (new_width + 1)
+
+    ascii_art = ""
+    for i in range(0, min(max_rows * img_width, len(img_str)), img_width):
+        ascii_art += img_str[i:i + img_width] + "\n"
+
+    return ascii_art
+
+
+# Функция преобразования пикселей изображения в ASCII-символы с пользовательским набором
+def pixels_to_ascii(image, ascii_chars):
+    pixels = image.getdata()
+    characters = ""
+    for pixel in pixels:
+        characters += ascii_chars[pixel * len(ascii_chars) // 256]
+    return characters
+
+# Функция огрубления изображения и отправки пользователю
+def pixelate_and_send(message):
+    photo_id = user_states[message.chat.id]['photo']
+    file_info = bot.get_file(photo_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    image_stream = io.BytesIO(downloaded_file)
+    image = Image.open(image_stream)
+    pixelated = pixelate_image(image, 20)
+
+    output_stream = io.BytesIO()
+    pixelated.save(output_stream, format="JPEG")
+    output_stream.seek(0)
+    bot.send_photo(message.chat.id, output_stream)
+
+# Функция огрубления изображения
+def pixelate_image(image, pixel_size):
+    image = image.resize(
+        (image.size[0] // pixel_size, image.size[1] // pixel_size),
+        Image.NEAREST
+    )
+    image = image.resize(
+        (image.size[0] * pixel_size, image.size[1] * pixel_size),
+        Image.NEAREST
+    )
+    return image
+
+# Запуск бота
+bot.infinity_polling()
